3f19d3db5fe06cf341f5a6e865b355b402f1e470
filipnedelkov <45723293+filipnedelkov@users.noreply.github.com>
2021-09-23 15:21:55 -0600

    Added PlaceMarker function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -18,6 +18,11 @@ void DisplayBoard(std::vector<std::vector<int>> board){
 	}
 }
 
+void PlaceMarker(int row, int col, std::vector<std::vector<int>> &board) {
+    // assuming row and col are in bounds of the CreateBoard
+    board[row][col] = 1;
+}
+
 int main() {
      std::vector<std::vector<int>> board = CreateBoard();
      DisplayBoard(board);
